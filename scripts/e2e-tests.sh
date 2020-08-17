39291c7319e832b49fea7d14cafa31a02b585b5e
arout <arout@redhat.com>
2020-08-17 21:41:26 +0530

    Adding more tests

--- scripts/e2e-tests.sh
+++ scripts/e2e-tests.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+# fail if some commands fails
 set -e
+# show commands
+set -x
 
-odo preference set Experimental true
-odo project create test -w
-odo create nodejs --devfile devfiles/nodejs/devfile.yaml
-odo push
+cd devfiles
+
+for dir in */; do
+    if [ -d "$dir" ]; then
+        TMP_DIR=$(mktemp -d)
+        export GLOBALODOCONFIG=$TMP_DIR/preference.yaml
+        odo preference set Experimental true
+        echo "$dir"
+        COMP_NAME=`echo $dir | sed 's:/*$::'`
+        PROJECT_NAME=`LC_CTYPE=C tr -dc a-z < /dev/urandom | head -c 10 | xargs`
+        odo project create $PROJECT_NAME -w
+        odo create $COMP_NAME --devfile $COMP_NAME/devfile.yaml --context $TMP_DIR
+        odo push --context $TMP_DIR
+        rm -rf $TMP_DIR
+    fi
+done
