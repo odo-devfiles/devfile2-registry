d7321f05d0c817f10cff6551c8ce848197ac4e95
Tomas Kral <tkral@redhat.com>
2020-11-13 14:33:04 +0100

    fix sample project for openliberty

--- scripts/test.sh
+++ scripts/test.sh
@@ -152,7 +152,7 @@ test() {
 #  - url path to check for response (usually "/")
 #  - string that url response must contain to checking that application is running corect 
 test "java-maven" "https://github.com/odo-devfiles/springboot-ex.git" "8080" "/" "You are currently running a Spring server built for the IBM Cloud"
-test "java-openliberty" "https://github.com/OpenLiberty/application-stack-starters.git" "9080" "/starter/resource" "Hello! Welcome to Open Liberty"
+test "java-openliberty" "https://github.com/OpenLiberty/application-stack-intro.git" "9080" "/api/resource" "Hello! Welcome to Open Liberty"
 test "java-quarkus" "https://github.com/odo-devfiles/quarkus-ex" "8080" "/" "Congratulations, you have created a new Quarkus application."
 test "java-springboot" "https://github.com/odo-devfiles/springboot-ex.git" "8080" "/" "You are currently running a Spring server built for the IBM Cloud"
 test "nodejs" "https://github.com/odo-devfiles/nodejs-ex.git" "3000" "/" "Hello from Node.js Starter Application!"
